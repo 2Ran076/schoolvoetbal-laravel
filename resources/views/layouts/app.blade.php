bcec89c3b140376a2fac1a18edd1d6b3e69ed17f
2Ran076 <D290119@edu.curio.nl>
2024-06-13 23:55:57 +0200

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
 </head>
 <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">Schoolvoetbal</a>
+        <a class="navbar-brand" href="/">Schoolvoetbal</a>
         <div class="collapse navbar-collapse">
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item">
