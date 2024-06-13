7b8bea567d33c76460c047b2e0aa4a8fdd9e2793
2Ran076 <D290119@edu.curio.nl>
2024-06-13 23:56:08 +0200

    updates

--- app/Models/Game.php
+++ app/Models/Game.php
@@ -5,26 +5,18 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-// In the Game model
 class Game extends Model
 {
-    public function team1()
-    {
-        return $this->belongsTo(Team::class, 'team1_id');
-    }
+    use HasFactory;
 
-    public function team2()
-    {
-        return $this->belongsTo(Team::class, 'team2_id');
-    }
-
-    public function referee()
-    {
-        return $this->belongsTo(User::class, 'referee_id');
-    }
-
-    public function goals()
-    {
-        return $this->hasMany(Goal::class, 'match_id');
-    }
+    // Specify the fields that are mass assignable
+    protected $fillable = [
+        'team1_id',
+        'team2_id',
+        'team1_score',
+        'team2_score',
+        'field',
+        'referee_id',
+        'time',
+    ];
 }
